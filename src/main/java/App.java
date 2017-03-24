f0ea7d9ebac0f9a58232ef4cb65a24d710c10798
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-03-24 14:47:50 -0700

    Add initial styling and images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {

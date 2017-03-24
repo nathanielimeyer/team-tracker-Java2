75b877d22c0c9d37dd301b65dda08ea11e2a295b
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-03-24 09:22:55 -0700

    Add all Member-independent Team methods and passing tests

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -3,11 +3,40 @@ import java.util.ArrayList;
 
 public class Team {
   private String mName;
-  private String mProjectGoal;
+  private String mGoal;
   private static List<Team> instances = new ArrayList<Team>();
   private int mId;
 
-  public Team(String name, String projectGoal){
-
+  public Team(String name, String goal){
+    mName = name;
+    mGoal = goal;
+    instances.add(this);
+    mId = instances.size();
   }
+
+  public String getName(){
+    return mName;
+  }
+
+  public String getGoal(){
+    return mGoal;
+  }
+
+  public static List<Team> all(){
+    return instances;
+  }
+
+  public static void clearTeams() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Team find(int id) {
+    return instances.get(id - 1);
+  }
+
+  
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -8,4 +8,46 @@ public class TeamTest {
     Team testTeam = new Team("", "");
     assertTrue(testTeam instanceof Team);
   }
+
+  @Test
+  public void getName_teamInstantiatesWithName_FieryPhoenix(){
+    Team testTeam = new Team("Fiery Phoenix", "Integrate app A with app B");
+    assertEquals("Fiery Phoenix", testTeam.getName());
+  }
+
+  @Test
+  public void getTeamGoal_teamInstantiatesWithName_FieryPhoenix(){
+    Team testTeam = new Team("Fiery Phoenix", "Integrate app A with app B");
+    assertEquals("Integrate app A with app B", testTeam.getGoal());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTeam_true(){
+    Team firstTeam = new Team("Fiery Phoenix", "Integrate app A with app B");
+    Team secondTeam = new Team("Cool Ranch", "Integrate Doritos with stomach");
+    assertTrue(Team.all().contains(firstTeam));
+    assertTrue(Team.all().contains(secondTeam));
+  }
+
+  @Test
+  public void clear_emptiesAllTeamsFromList_0(){
+    Team testTeam = new Team("Cool Ranch", "Integrate Doritos with stomach");
+    Team.clearTeams();
+    assertEquals(0, Team.all().size());
+  }
+
+  @Test
+  public void getId_teamInstantiatesWithAnID_1(){
+    Team.clearTeams();
+    Team testTeam = new Team("Cool Ranch", "Integrate Doritos with stomach");
+    assertEquals(1, testTeam.getId());
+  }
+
+  @Test
+  public void find_returnsTeamWithSameId_secondTeam(){
+    Team.clearTeams();
+    Team firstTeam = new Team("Fiery Phoenix", "Integrate app A with app B");
+    Team secondTeam = new Team("Cool Ranch", "Integrate Doritos with stomach");
+    assertEquals(Team.find(secondTeam.getId()), secondTeam);
+  }
 }
